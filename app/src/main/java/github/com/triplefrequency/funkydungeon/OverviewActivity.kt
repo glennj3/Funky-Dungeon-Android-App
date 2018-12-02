d16daf54d0080f08f079f34ced927a209526cb09
glennj3 <glennj3@nku.edu>
2018-12-01 19:16:25 -0500

    Tabs

--- app/src/main/java/github/com/triplefrequency/funkydungeon/OverviewActivity.kt
+++ app/src/main/java/github/com/triplefrequency/funkydungeon/OverviewActivity.kt
@@ -3,13 +3,12 @@ package github.com.triplefrequency.funkydungeon
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-import kotlinx.android.synthetic.main.activity_overview.*
+
 
 class OverviewActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_overview)
-        setActionBar(funky_toolbar as android.widget.Toolbar?)
     }
 }
