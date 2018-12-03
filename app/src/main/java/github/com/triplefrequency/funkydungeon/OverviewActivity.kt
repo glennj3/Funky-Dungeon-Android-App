9d9c2538aaa3a083cc28c1b538932e3b7e8eb3ca
glennj3 <glennj3@nku.edu>
2018-12-03 09:49:15 -0500

    Added button listeners

--- app/src/main/java/github/com/triplefrequency/funkydungeon/OverviewActivity.kt
+++ app/src/main/java/github/com/triplefrequency/funkydungeon/OverviewActivity.kt
@@ -2,13 +2,30 @@ package github.com.triplefrequency.funkydungeon
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-
+import android.widget.Button
 
 
 class OverviewActivity : AppCompatActivity() {
 
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        var btnOverview = findViewById<Button>(R.id.btn_overview)
+        var btnAttributes= findViewById<Button>(R.id.btn_attributes)
+        var btnSkills = findViewById<Button>(R.id.btn_skills)
+        var btnAttacks = findViewById<Button>(R.id.btn_attacks)
+
+        
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_overview)
+        btnOverview.setOnClickListener {}
+        btnAttributes.setOnClickListener {}
+        btnSkills.setOnClickListener {}
+        btnAttacks.setOnClickListener {}
+        //TODO Need to have a way to dynamically change values from storage
+
     }
+
+
 }
