602700251d58fc22434bdabdf57fd6b01ce72c10
Michael Haas <TripleFrequency@TripleFrequency.com>
2018-12-03 16:15:10 -0500

    Add documentation to CharacterSaver

--- app/src/main/java/github/com/triplefrequency/funkydungeon/repository/CharacterSaver.kt
+++ app/src/main/java/github/com/triplefrequency/funkydungeon/repository/CharacterSaver.kt
@@ -4,9 +4,14 @@ import com.google.firebase.firestore.FirebaseFirestore
 import github.com.triplefrequency.funkydungeon.model.Character
 
 class CharacterSaver {
+    /**
+     * The database to which data is being saved.
+     */
     private val db = FirebaseFirestore.getInstance()
 
+    /**
+     * Save the given [Character] to the default Firestore [db]
+     */
     fun save(character: Character) {
-
     }
 }
