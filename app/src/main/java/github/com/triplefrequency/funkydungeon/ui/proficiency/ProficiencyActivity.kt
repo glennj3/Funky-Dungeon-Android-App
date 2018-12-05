b3f80779d0ece1c2e3017b0ef79eaf949452eeb7
Alex Fish <TriumphantHero@gmail.com>
2018-12-04 21:58:41 -0500

    Potentially fixed layout

--- app/src/main/java/github/com/triplefrequency/funkydungeon/ui/proficiency/ProficiencyActivity.kt
+++ app/src/main/java/github/com/triplefrequency/funkydungeon/ui/proficiency/ProficiencyActivity.kt
@@ -1,23 +1,47 @@
 package github.com.triplefrequency.funkydungeon.ui.proficiency
 
-import android.os.Bundle
-import android.support.design.widget.Snackbar
-import android.support.v7.app.AppCompatActivity
-import github.com.triplefrequency.funkydungeon.R
 
-import kotlinx.android.synthetic.main.activity_proficiency.*
+import android.content.Intent
+import android.databinding.ObservableList
+import android.os.Bundle
+import android.support.design.widget.FloatingActionButton
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.support.v7.widget.Toolbar
+import github.com.triplefrequency.funkydungeon.R
+import github.com.triplefrequency.funkydungeon.core.Constants
+import github.com.triplefrequency.funkydungeon.repository.CharacterRepository
+import github.com.triplefrequency.funkydungeon.model.Character;
 
 class ProficiencyActivity : AppCompatActivity() {
+    private var proficiencyList: ObservableList<String>? = null
+    private var mRecyclerView: RecyclerView? = null
+    private var mAdapter: ProficiencyListAdapter? = null
+    private var character: Character? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_proficiency)
-        setSupportActionBar(toolbar)
-
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+        val profIntent = intent
+        if (profIntent != null) {
+            val id = profIntent.getStringExtra(Constants.ARG_CHARACTER_ID)
+            if (id != null) {
+                character = CharacterRepository.characters[id]
+                if (character != null) {
+                    proficiencyList = character!!.proficiencies
+                }
+            }
+        }
+        mRecyclerView = findViewById(R.id.prof_recyclerview)
+        mAdapter = ProficiencyListAdapter(this, proficiencyList)
+        mRecyclerView!!.adapter = mAdapter
+        mRecyclerView!!.layoutManager = LinearLayoutManager(this)
+        val fab = findViewById<FloatingActionButton>(R.id.prof_fab)
+        fab.setOnClickListener {
+            val wordListSize = proficiencyList!!.size
+            proficiencyList!!.add("New Proficiency")
+            mRecyclerView!!.adapter.notifyItemInserted(wordListSize)
+            mRecyclerView!!.smoothScrollToPosition(wordListSize)
         }
     }
-
 }
--- app/src/main/java/github/com/triplefrequency/funkydungeon/ui/proficiency/ProficiencyListAdapter.java
+++ app/src/main/java/github/com/triplefrequency/funkydungeon/ui/proficiency/ProficiencyListAdapter.java
@@ -3,6 +3,8 @@ package github.com.triplefrequency.funkydungeon.ui.proficiency;
 import android.content.Context;
 import android.databinding.ObservableList;
 import android.support.v7.widget.RecyclerView;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,23 +15,26 @@ import github.com.triplefrequency.funkydungeon.R;
 public class ProficiencyListAdapter extends RecyclerView.Adapter<ProficiencyListAdapter.ProficiencyViewHolder>{
     private final ObservableList<String> mProficiencyList;
     private LayoutInflater mInflater;
-    class ProficiencyViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+    class ProficiencyViewHolder extends RecyclerView.ViewHolder {
         public final EditText proficiencyItemView;
         final ProficiencyListAdapter mAdapter;
         public ProficiencyViewHolder(View itemView, ProficiencyListAdapter adapter) {
             super(itemView);
-            proficiencyItemView = itemView.findViewById(R.id.word);
+            proficiencyItemView = itemView.findViewById(R.id.prof_item);
+            proficiencyItemView.addTextChangedListener(new TextWatcher() {
+
+                public void afterTextChanged(Editable s) {
+                    int mPosition = getLayoutPosition();
+                    mProficiencyList.set(mPosition, proficiencyItemView.getText().toString());
+                }
+
+                public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+                public void onTextChanged(CharSequence s, int start, int before, int count) {}
+            });
             this.mAdapter = adapter;
-            itemView.setOnClickListener(this);
         }
 
-        @Override
-        public void onClick(View v) {
-            int mPosition = getLayoutPosition();
-            String element = mProficiencyList.get(mPosition);
-            mProficiencyList.set(mPosition, "Clicked! " + element);
-            mAdapter.notifyDataSetChanged();
-        }
     }
     public ProficiencyListAdapter(Context context,
                            ObservableList<String> proficiencyList) {
