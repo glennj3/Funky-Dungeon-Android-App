b2948b003c470a7623f76fea2c1cb170a38a4492
Michael Haas <TripleFrequency@TripleFrequency.com>
2018-12-05 10:32:46 -0500

    Improve proficiency activity

--- app/src/main/java/github/com/triplefrequency/funkydungeon/ui/proficiency/ProficiencyActivity.kt
+++ app/src/main/java/github/com/triplefrequency/funkydungeon/ui/proficiency/ProficiencyActivity.kt
@@ -1,22 +1,19 @@
 package github.com.triplefrequency.funkydungeon.ui.proficiency
 
 
-import android.content.Intent
 import android.databinding.ObservableList
 import android.os.Bundle
 import android.support.design.widget.FloatingActionButton
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
-import android.support.v7.widget.RecyclerView
-import android.support.v7.widget.Toolbar
 import github.com.triplefrequency.funkydungeon.R
 import github.com.triplefrequency.funkydungeon.core.Constants
-import github.com.triplefrequency.funkydungeon.repository.CharacterRepository
-import github.com.triplefrequency.funkydungeon.model.Character;
+import github.com.triplefrequency.funkydungeon.model.Character
+import github.com.triplefrequency.funkydungeon.model.CharacterContent
+import kotlinx.android.synthetic.main.content_proficiency.*
 
 class ProficiencyActivity : AppCompatActivity() {
     private var proficiencyList: ObservableList<String>? = null
-    private var mRecyclerView: RecyclerView? = null
     private var mAdapter: ProficiencyListAdapter? = null
     private var character: Character? = null
 
@@ -26,22 +23,21 @@ class ProficiencyActivity : AppCompatActivity() {
         if (profIntent != null) {
             val id = profIntent.getStringExtra(Constants.ARG_CHARACTER_ID)
             if (id != null) {
-                character = CharacterRepository.characters[id]
+                character = CharacterContent.characterMap[id]
                 if (character != null) {
-                    proficiencyList = character!!.proficiencies
+                    proficiencyList = character?.proficiencies
                 }
             }
         }
-        mRecyclerView = findViewById(R.id.prof_recyclerview)
         mAdapter = ProficiencyListAdapter(this, proficiencyList)
-        mRecyclerView!!.adapter = mAdapter
-        mRecyclerView!!.layoutManager = LinearLayoutManager(this)
+        prof_recyclerview.adapter = mAdapter
+        prof_recyclerview.layoutManager = LinearLayoutManager(this)
         val fab = findViewById<FloatingActionButton>(R.id.prof_fab)
         fab.setOnClickListener {
             val wordListSize = proficiencyList!!.size
             proficiencyList!!.add("New Proficiency")
-            mRecyclerView!!.adapter.notifyItemInserted(wordListSize)
-            mRecyclerView!!.smoothScrollToPosition(wordListSize)
+            prof_recyclerview.adapter.notifyItemInserted(wordListSize)
+            prof_recyclerview.smoothScrollToPosition(wordListSize)
         }
     }
 }
